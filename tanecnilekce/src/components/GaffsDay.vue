8c0d83229d77a20e57729fad85b8cd1b46af84a2
Jana Sekaninova <jana.sekaninova01@gmail.com>
2019-12-14 21:47:08 +0100

    Switch- změna čísla na dny u Gaffs results

--- tanecnilekce/src/components/GaffsDay.vue
+++ tanecnilekce/src/components/GaffsDay.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>{{ timetable.id }}</h2>
+    <h2>{{ nameOfDays}}</h2>
     <gaffsresults 
         v-for="(action, index) in timetable.actions"
         v-bind:singleGaff="action"
@@ -16,7 +16,29 @@ export default {
   components: {
     gaffsresults: GaffsResults
   },
-  props: ["timetable"]
+  props: ["timetable"],
+
+  computed: {
+    nameOfDays: function() {
+      switch(this.timetable.id) {
+        case 1:
+          return "Pondělí"
+        case 2: 
+        return "Uterý"
+        case 3:
+        return "Středa"
+        case 4:
+          return "Čtvrtek"
+        case 5:
+          return "Pátek"
+        case 6:
+          return "Sobota"
+        case 7: 
+        return "Neděle"        
+      }
+
+    }
+  }
 };
 </script>
 
